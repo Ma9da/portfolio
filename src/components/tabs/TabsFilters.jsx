26851145388dc40837d46f84df28a2d6cded1427
ma9da <dodymasry25@gmail.com>
2022-02-08 23:23:25 +0200

    editing tabs links

--- src/components/tabs/TabsFilters.jsx
+++ src/components/tabs/TabsFilters.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 
+
 const Tabsfilters = () => {
     return (
         <>
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    {/* <a class="nav-link active" aria-current="page" href="#">All</a> */}
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">All</button>
                 </li>
-                <li class="nav-item">
-                    {/* <a class="nav-link" href="#">Female</a> */}
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Women</button>
                 </li>
-                <li class="nav-item">
-                    {/* <a class="nav-link" href="#">Male</a> */}
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Men</button>
                 </li>
-                <li class="nav-item">
-                    {/* <a class="nav-link ">other</a> */}
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Jewelery</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Electronics</button>
                 </li>
             </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                </div>
+                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    <p>hello from tab 2</p>
+                </div>
+                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+            </div>
         </>
     );
 }
